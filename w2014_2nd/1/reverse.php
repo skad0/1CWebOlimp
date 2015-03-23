f8dfafc87d18276dc4cf5a10c51d1f6450cf0297
justnero-ru <justnero-ru@users.noreply.github.com>
2015-03-24 00:25:20 +0300

    Force binary mode

--- w2014_2nd/1/reverse.php
+++ w2014_2nd/1/reverse.php
@@ -4,11 +4,14 @@ ini_set('memory_limit','2M');
 if(isset($_FILES['file']) && $_FILES['file']['error'] == 0) {
 	$path = pathinfo($_FILES['file']['name']);
 	$ext = $path['extension'];
-	$in = fopen($_FILES['file']['tmp_name'],'r');
-	$out = fopen('out.'.$ext,'w');
+	$in = fopen($_FILES['file']['tmp_name'],'rb');
+	$out = fopen('out.'.$ext,'wb');
 	fseek($in,-1,SEEK_END);
 	do {
-		$tmp = fread($in,1);
+		if(($tmp = fread($in,1)) === FALSE) {
+			echo 'Error reading file'."\n";
+			break;
+		}
 		fwrite($out,$tmp);
 	} while(fseek($in,-2,SEEK_CUR) == 0);
 	fclose($in);
