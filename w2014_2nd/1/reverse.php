38b07989ba2cf4011418a495589eab8b423f343a
justnero-ru <justnero.ru@users.noreply.github.com>
2015-03-24 00:03:58 +0300

    Task one solved

--- w2014_2nd/1/reverse.php
+++ w2014_2nd/1/reverse.php
@@ -1,6 +1,18 @@
 <?php
-ini_set('memory_limit', '2M');
-$outfile_path = __DIR__.'/';
+ini_set('memory_limit','2M');
 
-var_dump($_FILES[0]['type']);
+if(isset($_FILES['file']) && $_FILES['file']['error'] == 0) {
+	$path = pathinfo($_FILES['file']['name']);
+	$ext = $path['extension'];
+	$in = fopen($_FILES['file']['tmp_name'],'r');
+	$out = fopen('out.'.$ext,'w');
+	fseek($in,-1,SEEK_END);
+	do {
+		$tmp = fread($in,1);
+		fwrite($out,$tmp);
+	} while(fseek($in,-2,SEEK_CUR) == 0);
+	fclose($in);
+	fclose($out);
+}
+
 ?>
